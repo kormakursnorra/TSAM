fdcf07578805d51d3830d4274a31786992954f92
kormakursnorra <kormakur02@gmail.com>
2026-09-16 21:33:30 +0000

    Refactor puzzlesolver: reorder functions and fix sendToPort buffer handling

--- Prog2/puzzlesolver.cpp
+++ Prog2/puzzlesolver.cpp
@@ -24,6 +24,91 @@
 const int MAX_RETRIES = 5;
 const int TIMEOUT_MS = 10;
 
+/* Sets the receive timout for the UDP socket,
+it is in milliseconds(ms)
+
+inputs: 
+ms: timeout duration in milliseconds.
+sockfd: socket to set the timeout on.
+Return:
+0 if the timeout is set successfully.
+-1 if an error occurs.
+
+*/
+int setSocketTimeout( int ms, const int sockfd )
+{
+    struct timeval tv;
+    tv.tv_sec = ms / 1000;
+    tv.tv_usec = ( ms % 1000 ) * 1000;
+
+    return setsockopt( sockfd, SOL_SOCKET, 
+                    SO_RCVTIMEO, &tv, 
+                    sizeof( tv ) );
+}
+
+
+/*
+
+*/
+int mapToPort( char* buff, const int port, std::map< std::string, int >& portMap )
+{
+    std::string buffContent =  static_cast< std::string >( buff );
+    
+    for ( const auto& [key, value] : portMap ) 
+    {
+        if( buffContent.find(key) != std::string::npos )
+        {
+            portMap[key] = port;
+        }
+    }
+}
+
+
+/*
+The sendToPort function scans a single UPD port by sending data 
+to the destination and waiting for a response. The function will try
+MAX_RETRIES times if no response is recieved
+inputs:
+port: the UDP port number to scan
+data: the data that is sent to the destination.
+Return:
+1 if a response is recieved and the port is considered open.
+0 if no response is recieved after all retries.
+-1 if an error occurs while sending or receiving data.
+*/
+int sendToPort( const int sockfd, const int port, std::string data, char* buff ) 
+{    
+    for( int attempt = 0; attempt < MAX_RETRIES; attempt++ )
+    {
+        ssize_t sent = send( sockfd, data.data(), data.length(), 0 ); 
+        
+        if( sent < 0 )
+        {
+            perror( "Error: Couldn't send data\n" );
+            return -1;
+        }
+
+        ssize_t received = recv( sockfd, buff, sizeof( buff ), 0 );
+
+        if( received < 0 )
+        {
+            // Check if resource is available or is blocking ( try again ) 
+            if( errno == EAGAIN || errno == EWOULDBLOCK )
+            {
+                continue;
+            }
+            
+            perror( "Error: No response\n" );
+            return -1;
+        }
+        
+        // Port is open, return 1
+        return 1;
+    }
+    
+    return 0;
+}
+
 /* Consturcts a "secret message" as a data packet and 
  to send to the open ports that request it. 
 
@@ -47,72 +132,9 @@ int constructMessage( uint32_t &secretNumber, std::string &secretMessage, const 
     return 0;
 }
 
-/* Sets the receive timout for the UDP socket,
-it is in milliseconds(ms)
+int sendToSecret( const int sockfd, const int port, std::string& message )
+{}
 
-inputs: 
-ms: timeout duration in milliseconds.
-sockfd: socket to set the timeout on.
-Return:
-0 if the timeout is set successfully.
--1 if an error occurs.
-
-*/
-int setSocketTimeout( int ms, const int sockfd )
-{
-    struct timeval tv;
-    tv.tv_sec = ms / 1000;
-    tv.tv_usec = ( ms % 1000 ) * 1000;
-
-    return setsockopt( sockfd, SOL_SOCKET, 
-                    SO_RCVTIMEO, &tv, 
-                    sizeof( tv ) );
-}
-
-/*
-The scanPort function scans a single UPD port by sending data 
-to the destination and waiting for a response. The function will try
-MAX_RETRIES times if no response is recieved
-inputs:
-port: the UDP port number to scan
-data: the data that is sent to the destination.
-Return:
-1 if a response is recieved and the port is considered open.
-0 if no response is recieved after all retries.
--1 if an error occurs while sending or receiving data.
-*/
-int sendToPort( const int sockfd, const int port, std::string data, char* reply ) 
-{    
-    for( int attempt = 0; attempt < MAX_RETRIES; attempt++ )
-    {
-        ssize_t sent = send( sockfd, data.data(), data.length(), 0 ); 
-        
-        if( sent < 0 )
-        {
-            perror( "Error: Couldn't send data\n" );
-            return -1;
-        }
-
-        ssize_t received = recv( sockfd, reply, sizeof( reply ), 0 );
-
-        if( received < 0 )
-        {
-            // Check if resource is available or is blocking ( try again ) 
-            if( errno == EAGAIN || errno == EWOULDBLOCK )
-            {
-                continue;
-            }
-            
-            perror( "Error: No response\n" );
-            return -1;
-        }
-        
-        // Port is open, return 1
-        return 1;
-    }
-    
-    return 0;
-}
 /*
 The Main function reads the IP Address and port range from
  the command line arguments, it creates the UDP socket and sets 
@@ -185,12 +207,18 @@ int main( int argc, char* argv[] )
         
         char buffer[2048];
         
-        int result = sendToPort( sockfd, port, "Hello", buffer );
-        if ( result < 0 )
+        if ( sendToPort( sockfd, port, "Hello", buffer ) < 0 )
         {
             std::cerr << "Error: Couldn't scan port: " << port << std::endl;
         }
+        
+        if( mapToPort( buffer, port, portMap) < 0 )
+        {
+            std::cerr << "Error: Couln't map to port: " << port << std::endl;
+        }
     }
+
+
 
     uint32_t secretNumber;     // Randomly generated, 32-bit secret number  
     const std::string userNames = "aroni21, bergurpb24, kormakur24"; // Usernames
